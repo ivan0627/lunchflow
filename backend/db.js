c08affaead61451191f862c000510779887fdfaa
Ivan <santiagog0627@gmail.com>
2024-04-24 22:31:46 -0500

    ready to first deploy with backend url

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "root",
-    host: "localhost",
+    user: "root",
+    password: "lo61SomUjtcU4vJfN8PC7ajglLKdhc1L",
+    host: "dpg-cokslsud3nmc739kuc4g-a.oregon-postgres.render.com",
     port: 5432,
     database: "lunchflow"
 });
